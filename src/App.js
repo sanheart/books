0318f25c79b002f0c7404a38d8a0bbe875640dcf
wangrui <wangrui.0630@outlook.com>
2018-06-05 16:54:00 +0800

    so hard

--- src/App.js
+++ src/App.js
@@ -1,40 +1,63 @@
-import React, { Component } from 'react'
-// import * as BooksAPI from './BooksAPI'
+import React from 'react'
+import {Route, Link} from 'react-router-dom'
+import * as BooksAPI from './BooksAPI'
 import './App.css'
-import { Route } from 'react-router-dom'
+import HeaderPart from './HeaderPart'
+// import Book from './Book'
 import BookShelf from './BookShelf'
 import SearchBook from './SearchBook'
-import * as BooksAPI from './BooksAPI'
 
-class App extends Component {
+class BooksApp extends React.Component {
   state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    //showSearchPage: false
-    books: [],
-    //serachedBooks: []
-
+    // 书堆数组
+    books_array: []
   }
 
+  moveBook = (book, shelf) => {
+    // 若书堆中有书，即书堆不为空时
+    // 由BookAPI的update()中的fetch方式来异步获取更新数据
+    if (this.state.books_array) {
+      BooksAPI.update(book,shelf).then(() => {
+        book.shelf = shelf;
+        this.setState(state => ({
+          books_array: state.books_array.filter(oneBook => oneBook.id !== book.id).concat([ book ])
+        }))
+      })
+    }
+  }
+
+  // 执行componentDidMount()钩子时异步获取数据
   componentDidMount() {
-    BooksAPI.getAll().then((books) => {
-      this.setState({ books })
+    BooksAPI.getAll().then((books_array) => {
+      this.setState({ books_array })
     })
   }
 
   render() {
     return (
-      <div className='app'>
-        <Route exact path='/' component={BookShelf}/>
-        <Route exact path='/search' component={SearchBook}/>
-      </div>
-    )
+      <div className="app">
+        {/* 搜索页Route */}
+        <Route path="/search" render={() => (
+          // 仅显示搜索组件
+          <SearchBook onMoveBook={this.moveBook} booksOnShelf={this.state.books_array}/>
+        )} />
+        {/* 应用首页Route */}
+        <Route exact path="/" render={() => (
+          <div className="list-books">
+            {/* 顶部栏组件 */}
+            <HeaderPart/>
+            <div className="list-books-content">
+              {/* 图书架分类（三类），其中含单个图书的组件 */}
+              <BookShelf onMoveBook={this.moveBook} booksOnShelf={this.state.books_array}/>
+            </div>
+            {/* 搜索按钮 */}
+            <div className="open-search">
+              <Link to="/search">Add a book</Link>
+            </div>
+          </div>
+        )} />
+    </div>)
   }
 }
 
-
-export default App
+export default BooksApp
